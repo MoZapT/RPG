4c3c5a805cd05f971c3e076f89dc5bf804ad4eda
MoZapT <45381702+MoZapT@users.noreply.github.com>
2024-06-02 12:22:29 +0200

    settings added

--- RPG/obj/Debug/net6.0/RPG.AssemblyInfo.cs
+++ RPG/obj/Debug/net6.0/RPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+494b2b016f485d0313569ab8899398a503fed4c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e3d38050c96c4578a5640f1df7d6a479c0472c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
